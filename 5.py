980fbf821ab2ac3c8183ebd49a1908430ac1c69b
Enmanuel Domínguez <manuelx_05@hotmail.com>
2023-04-08 15:10:27 -0400

    fechas

--- 5.py
+++ 5.py
@@ -1,6 +1,7 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-# Leer el archivo CSV y convertirlo en un DataFrame
+# Cargar el conjunto de datos
 df = pd.read_csv('SP500_data.csv')
 
 # Convertir la columna "Date" a datetime
@@ -9,8 +10,30 @@ df['Date'] = pd.to_datetime(df['Date'])
 # Extraer el año de la columna "Date"
 df['Year'] = df['Date'].dt.year
 
-# Agrupar los datos por año y calcular la media de la columna "Price"
-avg_price_by_year = df.groupby('Year')['Price'].mean()
+# Calcular estadísticas descriptivas
+print(df.describe())
 
-# Mostrar el precio promedio por año
-print(avg_price_by_year)
+# Filtrar datos de un rango específico de años
+start_year = 2000
+end_year = 2020
+filtered_df = df[(df['Year'] >= start_year) & (df['Year'] <= end_year)]
+
+# Calcular el promedio anual de la columna "SP500"
+average_yearly_SP500 = filtered_df.groupby('Year')['SP500'].mean()
+
+# Mostrar el promedio anual de la columna "SP500"
+print(average_yearly_SP500)
+
+# Visualizar la evolución del SP500 a lo largo del tiempo
+plt.plot(filtered_df['Date'], filtered_df['SP500'])
+plt.xlabel('Date')
+plt.ylabel('SP500')
+plt.title('Evolución del SP500 (2000-2020)')
+plt.show()
+
+# Visualizar la relación entre el SP500 y la tasa de interés a largo plazo
+plt.scatter(filtered_df['SP500'], filtered_df['Long Interest Rate'])
+plt.xlabel('SP500')
+plt.ylabel('Long Interest Rate')
+plt.title('Relación entre el SP500 y la tasa de interés a largo plazo (2000-2020)')
+plt.show()
